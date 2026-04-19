7af9c90a2943ddb70fee4bb60b333d31c415ad46
caroline.chaim <caroline.chaim@gmail.com>
2026-04-19 18:42:42 -0300

    initial commit

---
+++ server.js
@@ -0,0 +1,33 @@
+require('dotenv').config(); 
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+const TOKEN = process.env.TOKEN;
+
+app.get('/thing', async (req, res) => {
+  const id = req.query.id;
+  const response = await axios.get(
+    `https://api.geekdo.com/api/geekitems?objectid=${id}&objecttype=thing`,
+    { headers: { Authorization: `Bearer ${TOKEN}` } }
+  );
+  res.send(response.data);
+});
+
+app.get('/imagem', async (req, res) => {
+  try {
+    const url = req.query.url;
+    const response = await axios.get(url, { responseType: 'arraybuffer' });
+    const contentType = response.headers['content-type'];
+    res.set('Content-Type', contentType);
+    res.send(response.data);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+
+app.listen(3000, () => console.log('Servidor rodando em http://localhost:3000'));
